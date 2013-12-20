dec32b643fbb498535db6bfa95b612b3af37771e
Bjorn Wallner <bjornw@ifm.liu.se>
2013-12-20 14:32:27 +0100

    d

--- resampling/resampling.pl
+++ resampling/resampling.pl
@@ -10,4 +10,6 @@ $basename=$ARGV[0];
 $repack_cmd="$rosetta_path/relax.linuxgccrelease  -database $rosetta_db -in:file:fullatom -out:file:silent_struct_type binary -relax:membrane -membrane:Membed_init -score:weights membrane_highres.wts -in:file:spanfile $basename.span -nstruct 10 -relax_script $SCRIPT_DIR/resampling/repack.script -in:file:s *.pdb -out:file:silent ProQM.repacked.silent";
 $score_cmd="$rosetta_path/score.linuxgccrelease -database $rosetta_db -in:file:fullatom -ProQ:membrane -score:weights ProQM -in:file:silent ProQM.repacked.silent -out:file:scorefile ProQM.repacked.silent.score";
 print $repack_cmd."\n";
-print $score_cmd."\n"
+print $score_cmd."\n";
+system("$repack_cmd");
+system("$score_cmd");
