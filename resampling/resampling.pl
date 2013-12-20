8441b7a1114e4a7b7ab01fc44d5938f417ca0350
Bjorn Wallner <bjornw@ifm.liu.se>
2013-12-20 14:31:13 +0100

    updated resampling.pl

--- resampling/resampling.pl
+++ resampling/resampling.pl
@@ -1,7 +1,13 @@
 #!/usr/bin/perl -w
+use Cwd 'abs_path';
+use File::Basename;
+my $SCRIPT_DIR=dirname(abs_path($0));
 
+$rosetta_path="/home/x_bjowa/nobackup/git/rosetta_3.5/rosetta_source/bin/";
+$rosetta_db="/home/x_bjowa/nobackup/git/rosetta_3.5/rosetta_database/";
 
-   relax.linuxgccrelease  -database <rosetta database> -in:file:fullatom -out:file:silent_struct_type binary -relax:membrane -membrane:Membed_init -score:weights membrane_highres.wts -in:file:spanfile $basename.span -nstruct 10 -relax_script <dir_ProQ_scripts>/resampling/repack.script -in:file:s *.pdb -out:file:silent ProQM.repacked.silent
-
-Score each of these with ProQM:
-    score.linuxgccrelease -database <rosetta database> -in:file:fullatom -ProQ:membrane -score:weights ProQM -in:file:silent ProQM.repacked.silent -out:file:scorefile ProQM.repacked.silent.score
+$basename=$ARGV[0];
+$repack_cmd="$rosetta_path/relax.linuxgccrelease  -database $rosetta_db -in:file:fullatom -out:file:silent_struct_type binary -relax:membrane -membrane:Membed_init -score:weights membrane_highres.wts -in:file:spanfile $basename.span -nstruct 10 -relax_script $SCRIPT_DIR/resampling/repack.script -in:file:s *.pdb -out:file:silent ProQM.repacked.silent";
+$score_cmd="$rosetta_path/score.linuxgccrelease -database $rosetta_db -in:file:fullatom -ProQ:membrane -score:weights ProQM -in:file:silent ProQM.repacked.silent -out:file:scorefile ProQM.repacked.silent.score";
+print $repack_cmd."\n";
+print $score_cmd."\n"
