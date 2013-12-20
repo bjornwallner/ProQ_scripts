579a291454ed3c6bb213e15ac3780e6f79a33fa3
Bjorn Wallner <bjornw@ifm.liu.se>
2013-12-20 15:00:51 +0100

    final test

--- resampling/resampling.pl
+++ resampling/resampling.pl
@@ -10,10 +10,10 @@ $run_all_external_cmd="$SCRIPT_DIR/../bin/run_all_external.pl -pdb $basename -me
 
 $repack_cmd="$rosetta_path/relax.linuxgccrelease  -database $rosetta_db -in:file:fullatom -out:file:silent_struct_type binary -relax:membrane -membrane:Membed_init -score:weights membrane_highres.wts -in:file:spanfile $basename.span -nstruct 10 -relax:script $SCRIPT_DIR/repack.script -in:file:s *.pdb -out:file:silent ProQM.repacked.silent";
 $score_cmd="$rosetta_path/score.linuxgccrelease -database $rosetta_db -in:file:fullatom -ProQ:basename $basename -ProQ:membrane -score:weights ProQM -in:file:silent ProQM.repacked.silent -out:file:scorefile ProQM.repacked.silent.score";
-print $repack_cmd."\n";
-print $score_cmd."\n";
-#system("$run_all_external_cmd");
-#system("$repack_cmd");
-#system("$score_cmd");
-$selected=`grep ^SCORE ProQM.repacked.silent.score |sort -n -r -k 2|head|awk '{print \$7}'`;
+#print $repack_cmd."\n";
+#print $score_cmd."\n";
+system("$run_all_external_cmd");
+system("$repack_cmd");
+system("$score_cmd");
+$selected=`grep ^SCORE ProQM.repacked.silent.score |sort -n -r -k 2|head -n 1|awk '{print \$7}'`;
 print "Selected model: $selected";
