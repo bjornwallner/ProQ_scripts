96794903d4cee9652c8306087698144823cf46cd
Bjorn Wallner <bjornw@ifm.liu.se>
2015-01-05 11:15:22 +0100

    added integration test for feature copy 2

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x 
 
-mkdir test.$$
-cp tests_clean/1e12A_0001.pdb test.$$/
-bin/run_all_external.pl -pdb test.$$/1e12A_0001.pdb 
-cp tests_clean/1e12A_0001.subset.pdb test.$$/
-bin/copy_features_from_master.pl test.$$/1e12A_0001.subset.pdb test.$$/1e12A_0001.pdb
+num=$$
+
+mkdir test.$num
+cp tests_clean/1e12A_0001.pdb test.$num/
+bin/run_all_external.pl -pdb test.$num/1e12A_0001.pdb 
+cp tests_clean/1e12A_0001.subset.pdb test.$num/
+bin/copy_features_from_master.pl test.$num/1e12A_0001.subset.pdb test.$num/1e12A_0001.pdb
 
 #echo Membrane
 #bin/run_all_external.pl -pdb test.$$/1e12A_0001.pdb -membrane 1
